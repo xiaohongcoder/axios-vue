7e0ffd775da7bcdda13978e6fb95ec4cbb03edb9
xiaohongcoder <s2007722@gmail.com>
2021-07-30 16:59:04 +0800

    put & patch

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,14 @@ const routes = [
     component: Home
   },
   {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
     path: '/2_2',
     name: '2_2',
     component: () => import('../views/2_2.vue')
@@ -21,13 +29,10 @@ const routes = [
     component: () => import('../views/2_3.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/2_3_put_patch',
+    name: '2_3_put_patch',
+    component: () => import('../views/2_3_put_patch.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/2_3_put_patch.vue
@@ -0,0 +1,41 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "axios2_3_put_patch",
+  components: {},
+
+  created() {
+    // form-data 表单提交（图片上传，文件上传）
+    // application/json
+    let data = {
+      id: 12,
+    };
+
+    // put
+    axios.put("/put", data).then((res) => {
+      console.log("put", res);
+    });
+
+    // patch
+    axios.patch("/patch", data).then((res) => {
+      console.log("patch", res);
+    });
+
+    // form-data 请求
+    // Content-Type: multipart/form-data;
+    //   boundary=----WebKitFormBoundaryK5x3fzA72aKl2vN3
+    // let formData = new FormData();
+    // for (let key in data) {
+    //   formData.append(key, data[key]);
+    // }
+    // axios.post("/post", formData).then((res) => {
+    //   console.log(res);
+    // });
+  },
+};
+</script>
