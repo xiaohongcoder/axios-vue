53b82e0afd739104e2d85390531119a1e77ed67d
xiaohongcoder <s2007722@gmail.com>
2021-07-31 17:31:34 +0800

    添加、编辑联系人

--- src/views/contactList.vue
+++ src/views/contactList.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="contact-list">
-    <!-- 联系人列表 -->
     <van-contact-list :list="list" @add="onAdd" @edit="onEdit" />
+
+    <van-cell is-link @click="showPopup">展示弹出层</van-cell>
+    <van-popup v-model="show" :style="{ width: '90%' }">
+      <!-- 联系人编辑 -->
+      <van-contact-edit
+        is-edit
+        show-set-default
+        :contact-info="editingContact"
+        set-default-label="设为默认联系人"
+        @save="onSave"
+        @delete="onDelete"
+      />
+    </van-popup>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import { ContactList } from "vant";
-import { Toast } from "vant";
+import { ContactList, Toast, ContactEdit } from "vant";
+import { Popup } from "vant";
 
 export default {
   name: "contactList",
 
   components: {
     [ContactList.name]: ContactList,
+    [ContactEdit.name]: ContactEdit,
+    [Popup.name]: Popup,
   },
 
   data() {
@@ -23,10 +37,23 @@ export default {
 
       // axios 实例
       instance: null,
+
+      // 编辑弹窗的 显隐
+      showEdit: false,
+
+      // 正在编辑的联系人数据
+      editingcontact: {},
+
+      // 新建 or 编辑
+      // false 编辑 --- true 新建
+      isEdit: false,
+
+      show: false,
     };
   },
 
   created() {
+    console.log("created");
     this.instance = axios.create({
       baseURL: "http://localhost:9000/api",
       timeout: 1000,
@@ -40,15 +67,36 @@ export default {
       })
       .catch((err) => {
         console.log(err);
-        Toast('请稍后重试')
+        Toast("请稍后重试");
       });
   },
 
   methods: {
+    showPopup() {
+      this.show = true;
+    },  
+
     // 添加联系人
-    onAdd() {},
+    onAdd() {
+      console.log("onAdd");
+      this.showEdit = true;
+      this.isEdit = false;
+    },
+
     // 编辑联系人
-    onEdit() {},
+    onEdit(info) {
+      console.log("onEdit");
+      this.show = true;
+
+      this.showEdit = true;
+      this.isEdit = true;
+      this.editingcontact = info;
+    },
+
+    // 保存联系人
+    onSave() {},
+    // 删除联系人
+    onDelete() {},
   },
 };
 </script>
