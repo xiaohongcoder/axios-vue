eea37438acff84b97850dcde3d0b26fc0456bf94
xiaohongcoder <s2007722@gmail.com>
2021-07-31 15:28:11 +0800

    接口的调试

--- src/views/contactList.vue
+++ src/views/contactList.vue
@@ -1,19 +1,43 @@
 <template>
-  <div>
-    <van-button type="warning">警告按钮</van-button>
+  <div class="contact-list">
+    <!-- 联系人列表 -->
+    <van-contact-list :list="list" @add="onAdd" @edit="onEdit" />
   </div>
 </template>
 
 <script>
 // import axios from "axios";
-import { Button } from "vant";
+// import { Button } from "vant";
+import Vue from "vue";
+import { ContactList } from "vant";
+
+Vue.use(ContactList);
 
 export default {
   name: "contactList",
   components: {
-    [Button.name]: Button,
+    // [Button.name]: Button,
+    [ContactList.name]: ContactList,
+  },
+  data() {
+    return {
+      list: [
+        {
+          id: 1,
+          name: "name",
+          tel: "10010",
+          isDefault: false,
+        },
+      ],
+    };
   },
   created() {},
+  methods: {
+    // 添加联系人
+    onAdd() {},
+    // 编辑联系人
+    onEdit() {},
+  },
 };
 </script>
 
